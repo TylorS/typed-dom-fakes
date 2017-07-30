f436bcca8f7ea8db73186ab2a584beb15f51ab52
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-07-30 12:52:23 -0400

    feat(ArrayLike): implement findIndex, filter, and remove
    findIndex and filter are both immutable operations
    remove mutates the arrayLike

--- src/helpers/ArrayLike.test.ts
+++ src/helpers/ArrayLike.test.ts
@@ -1,4 +1,4 @@
-import { Test, describe, it } from '@typed/test'
+import { Test, describe, given, it } from '@typed/test'
 
 import { ArrayLike } from './ArrayLike'
 
@@ -76,5 +76,109 @@ export const test: Test = describe(`ArrayLike`, [
 
       equal(4, arrayLike.length)
     })
+  ]),
+
+  describe(`findIndex`, [
+    given(`a predicate function`, [
+      it(`returns -1 if not found`, ({ equal }) => {
+        const arrayLike = new ArrayLike()
+
+        const index = arrayLike.findIndex(x => x === 0)
+
+        equal(-1, index)
+      }),
+
+      it(`returns index a value is as when found`, ({ equal }) => {
+        const arrayLike = new ArrayLike()
+
+        arrayLike[0] = 0
+        arrayLike[1] = 100
+
+        const index = arrayLike.findIndex(x => x === 100)
+
+        equal(1, index)
+      })
+    ])
+  ]),
+
+  describe(`remove`, [
+    given(`an index`, [
+      it(`removes an item`, ({ equal }) => {
+        const arrayLike = new ArrayLike<number>()
+
+        arrayLike[0] = 0
+        arrayLike[1] = 1
+
+        arrayLike.remove(1)
+
+        equal(void 0, arrayLike[1])
+      }),
+
+      it(`moves following items to the left`, ({ equal }) => {
+        const arrayLike = new ArrayLike<number>()
+
+        arrayLike[0] = 1
+        arrayLike[1] = 2
+        arrayLike[2] = 3
+        arrayLike[3] = 4
+        arrayLike[4] = 5
+
+        equal(5, arrayLike.length)
+
+        arrayLike.remove(2)
+
+        equal(1, arrayLike[0])
+        equal(2, arrayLike[1])
+        equal(4, arrayLike[2])
+        equal(5, arrayLike[3])
+
+        equal(4, arrayLike.length)
+      })
+    ]),
+
+    given(`an index and a count`, [
+      it(`removes n number of items`, ({ equal }) => {
+        const arrayLike = new ArrayLike<number>()
+        const index = 2
+        const count = 2
+
+        arrayLike[0] = 0
+        arrayLike[1] = 10
+        arrayLike[2] = 20
+        arrayLike[3] = 30
+        arrayLike[4] = 40
+        arrayLike[5] = 50
+
+        equal(6, arrayLike.length)
+
+        arrayLike.remove(index, count)
+
+        equal(0, arrayLike[0])
+        equal(10, arrayLike[1])
+        equal(40, arrayLike[2])
+        equal(50, arrayLike[3])
+        equal(4, arrayLike.length)
+      })
+    ])
+  ]),
+  describe(`filter`, [
+    given(`a predicate`, [
+      it(`returns a new ArrayLike with only values passing predicate`, ({ equal }) => {
+        const a = new ArrayLike<number>()
+        const isEven = (value: number) => value % 2 === 0
+
+        a[0] = 1
+        a[1] = 2
+        a[2] = 3
+        a[3] = 4
+        a[4] = 5
+
+        const b = a.filter(isEven)
+
+        equal(2, b[0])
+        equal(4, b[1])
+        equal(2, b.length)
+      })
+    ])
   ])
 ])
--- src/helpers/ArrayLike.ts
+++ src/helpers/ArrayLike.ts
@@ -1,9 +1,9 @@
-import { keys, length } from '167'
+import { findIndex, keys, length } from '167'
 
 export class ArrayLike<A> {
   [index: number]: A
 
-  protected _length: number
+  protected _length: number;
 
   get length(): number {
     if (this._length) return this._length
@@ -24,15 +24,48 @@ export class ArrayLike<A> {
 
     const indexesToRemove = currentLength - newLength
 
-    for (let i = 0; i < indexesToRemove; ++i)
-      this[i + newLength] = void 0
+    for (let i = 0; i < indexesToRemove; ++i) this[i + newLength] = void 0
   }
-
-  // convenience methods
 
   public push(value: A) {
     if (this._length) this._length++
-    
+
     this[this.length] = value
   }
+
+  public findIndex(predicate: (value: A) => boolean): number {
+    return findIndex(predicate, this)
+  }
+
+  /*
+   * Does not support negative values for `count`.
+   */
+  public remove(index: number, count: number = 1): void {
+    const currentLength = this.length
+
+    if (currentLength > index) {
+      for (let i = index; i < currentLength; ++i) {
+        this[i] = this[i + count]
+        this[i + count] = void 0
+      }
+    } else {
+      this[index] = void 0
+    }
+
+    this._length = currentLength - count
+  }
+
+  public filter(predicate: (value: A, index: number) => boolean): ArrayLike<A> {
+    const currentLength = this.length
+    const arrayLike = new ArrayLike<A>()
+
+    for (let i = 0; i < currentLength; ++i) {
+      const value = this[i]
+
+      if (predicate(value, i))
+        arrayLike.push(value)
+    }
+
+    return arrayLike
+  }  
 }
