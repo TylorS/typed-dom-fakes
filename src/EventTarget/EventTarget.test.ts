733e48640c9af0d279ed9d2d4d958925a3ff1a62
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-07-30 17:12:22 -0400

    feat(EventTarget): implement EventTarget interface

---
+++ src/EventTarget/EventTarget.test.ts
@@ -0,0 +1,72 @@
+import { Test, describe, it } from '@typed/test'
+
+import { EventTargetImpl } from './EventTarget'
+
+export const test: Test = describe(`EventTarget`, [
+  it(`implements EventTarget interface`, ({ equal }) => {
+    const event: EventTarget = new EventTargetImpl()
+
+    equal('function', typeof event.addEventListener)
+    equal('function', typeof event.removeEventListener)
+    equal('function', typeof event.dispatchEvent)
+  }),
+
+  describe(`addEventListener`, [
+    it(`registers an event listener`, ({ equal }, done) => {
+      const type = 'click'
+      const event = { type, bubbles: false } as Event
+
+      function listener(ev: Event) {
+        equal(event, ev)
+        done()
+      }
+
+      const target: EventTarget = new EventTargetImpl()
+
+      target.addEventListener(type, listener)
+
+      target.dispatchEvent(event)
+    }),
+  ]),
+
+  describe(`dispatchEvent`, [
+    it(`calls event listener`, ({ equal }, done) => {
+      const type = 'click'
+      const event = { type, bubbles: false } as Event
+
+      function listener(ev: Event) {
+        equal(event, ev)
+        done()
+      }
+
+      const target: EventTarget = new EventTargetImpl()
+
+      target.addEventListener(type, listener)
+
+      target.dispatchEvent(event)
+    }),
+  ]),
+
+  describe(`removeEventListener`, [
+    it(`removes a previously added event listerner`, ({ ok }, done) => {
+      const type = 'click'
+      const event = { type, bubbles: false } as Event
+
+      function listener() {
+        done(new Error('Should not call listener'))
+      }
+
+      const target: EventTarget = new EventTargetImpl()
+
+      target.addEventListener(type, listener)
+
+      target.removeEventListener(type, listener)
+
+      ok(true)
+
+      target.dispatchEvent(event)
+
+      setTimeout(done)
+    }),
+  ]),
+])
---
+++ src/EventTarget/EventTarget.ts
@@ -0,0 +1,82 @@
+export interface FakeEvent extends Event {
+  propagationStopped: boolean
+  defaultPrevented: boolean
+}
+
+export class EventTargetImpl implements EventTarget {
+  protected bubble: Map<string, Set<EventListenerOrEventListenerObject>> = new Map()
+  protected capture: Map<string, Set<EventListenerOrEventListenerObject>> = new Map()
+
+  public addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    useCapture: boolean = false
+  ): void {
+    const { bubble, capture } = this
+
+    const eventMap = useCapture ? capture : bubble
+
+    const eventListeners: Set<EventListenerOrEventListenerObject> =
+      eventMap.get(type) || new Set<EventListenerOrEventListenerObject>()
+
+    eventListeners.add(listener)
+
+    if (!eventMap.has(type)) eventMap.set(type, eventListeners)
+  }
+
+  public removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    useCapture: boolean = false
+  ): void {
+    const { bubble, capture } = this
+
+    const eventMap = useCapture ? capture : bubble
+
+    const eventListeners = eventMap.get(type)
+
+    if (!eventListeners) return void 0
+
+    eventListeners.delete(listener)
+  }
+
+  public dispatchEvent(event: FakeEvent): boolean {
+    return this.dispatchToCaptureListeners(event) || this.dispatchToBubbleListeners(event)
+  }
+
+  protected dispatchToCaptureListeners(event: FakeEvent): boolean {
+    const listeners = this.capture.get(event.type)
+
+    if (!listeners) return false
+
+    deferListenersCallback(listeners, callEventListeners(event))
+
+    return true
+  }
+
+  protected dispatchToBubbleListeners(event: FakeEvent): boolean {
+    const listeners = this.bubble.get(event.type)
+
+    if (!listeners) return false
+
+    deferListenersCallback(listeners, callEventListeners(event))
+
+    return true
+  }
+}
+
+function deferListenersCallback(
+  listeners: Set<EventListenerOrEventListenerObject>,
+  f: (listener: EventListenerOrEventListenerObject) => any
+) {
+  Promise.resolve().then(() => listeners.forEach(f))
+}
+
+function callEventListeners(event: FakeEvent) {
+  return function callEventListener(listener: EventListenerOrEventListenerObject): void {
+    if (event.propagationStopped) return void 0
+
+    if (typeof listener === 'object') listener.handleEvent(event)
+    else listener(event)
+  }
+}
