fdf4f94aeb416c47378aebb6ddec6eb881ad6f6f
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-07-30 23:41:53 -0400

    feat(index): export all implementations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+// folders
+export * from './Element'
 export * from './EventTarget'
 export * from './Node'
+
+// files
+export * from './Attr'
+export * from './DOMTokenList'
+export * from './HTMLCollection'
 export * from './NamedNodeMap'
 export * from './NodeList'
